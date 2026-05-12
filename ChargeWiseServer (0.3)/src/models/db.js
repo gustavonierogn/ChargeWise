f50b3433798750ec5479c564c32794ee69be3375
gustavonierogn <gustavonierogn@gmail.com>
2026-05-12 14:52:47 -0300

    maria db

--- ChargeWiseServer (0.3)/src/models/db.js
+++ ChargeWiseServer (0.3)/src/models/db.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('chargewise', 'root', 'guga1410', {
+
+//para usar localhost no meu pc
+/* const sequelize = new Sequelize('chargewise', 'root', 'guga1410', {
+    host: "localhost",
+    dialect: 'mysql'
+}); */
+
+const sequelize = new Sequelize('chargewise', 'chargewise', 'senhachargewise', {
     host: "localhost",
     dialect: 'mysql'
 });
 
 sequelize.authenticate().then(function(){
     console.log("Conectado no banco de dados com sucesso!");
-}
-).catch(function(erro){
+}).catch(function(erro){
     console.log("Erro ao conectar no banco de dados:", erro);
-})
+});
 
-module.exports = sequelize
+module.exports = sequelize;
